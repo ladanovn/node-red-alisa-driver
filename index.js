aeea28e618057d16e3c0b4f46e40d960c2669767
ladanov_n <ladanov_n@mail.ru>
2018-11-14 10:25:17 +0300

    basic

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const axios = require('axios');
+
 module.exports = async function (RED) {
 
     function AlisaDriverNode(config) {
@@ -6,6 +9,76 @@ module.exports = async function (RED) {
         const node = this;
         const nodeContext = node.context();
 
+        const app = express();
+
+        app.post('/', async function (req, res) {
+
+            if (req.body.request.command == "no text") {
+                res.json({
+                    version: req.body.version,
+                    session: req.body.session,
+                    response: {
+                        text: "",
+                        end_session: false,
+                    },
+                });
+            } else if (req.body.request.command == "no version") {
+                res.json({
+                    session: req.body.session,
+                    response: {
+                        text: req.body.request.command || 'Hello!',
+                        end_session: false,
+                    },
+                });
+            } else if (req.body.request.command == "no session") {
+                res.json({
+                    version: req.body.version,
+                    response: {
+                        text: req.body.request.command || 'Hello!',
+                        end_session: false,
+                    },
+                });
+            } else if (req.body.request.command == "end session") {
+                res.json({
+                    version: req.body.version,
+                    session: req.body.session,
+                    response: {
+                        text: req.body.request.command || 'Hello!',
+                        end_session: true,
+                    },
+                });
+            } else {
+                const {
+                    parser_host,
+                    parser_port
+                } = config;
+
+                try {
+                    const request = await axios.post(`http://${parser_host}:${parser_port}/`, {
+                        sender: {
+                            type: 'Alisa'
+                        },
+                        source_text: req.body.request.command
+                    });
+                    console.log(request)
+                } catch (error) {
+                    res.json({
+                        version: req.body.version,
+                        session: req.body.session,
+                        response: {
+                            text: 'Server error',
+                            end_session: false
+                        }
+                    })
+                }
+            }
+        });
+
+        app.use('*', function (req, res) {
+            res.sendStatus(404);
+        });
+
+        app.listen(config.port);
     }
 
     RED.nodes.registerType("Alisa-driver", AlisaDriverNode);
